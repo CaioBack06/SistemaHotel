35ca826773f896259ca7eea054d08403c89ec7f1
CaioBack06 <161071713+CaioBack06@users.noreply.github.com>
2024-10-23 19:29:20 -0300

    Add files via upload

---
+++ src/sistemaHotel/Cadastro.java
@@ -0,0 +1,15 @@
+package sistemaHotel;
+
+public class Cadastro {
+
+	public static void main(String[] args) {
+		String tipoQ;
+		int nmQ;
+		float precoQ;
+		boolean disponibilidade;
+		
+		public void 
+
+	}
+
+}
---
+++ src/sistemaHotel/Reservar.java
@@ -0,0 +1,10 @@
+package sistemaHotel;
+
+public class Reservar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
